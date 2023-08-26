85380716001d95a040a631d608143f2cc1623ae7
Iamneo <techadmin@iamneo.ai>
2023-08-26 09:45:09 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controllers/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controllers/UserController.java
@@ -1,7 +1,14 @@
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+
 @RestController
 @RequestMapping
 public class UserController {
     
-   
+   @PostMapping("/saveTask")
+   public void newTask(@RequestBody task){
+
+   }
 
 }
